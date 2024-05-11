9580145a1f9b0d9372e089a7ee352babdd1c9350
andreyvegaaa <andreyvega411@gmail.com>
2024-05-11 11:42:50 +0800

    niceadmin successfuly integrated by adv

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,50 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div class="col-12">
+            <x-input-label for="name" :value="__('Name')" class="form-label"/>
+            <x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="invalid-feedback" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="col-12">
+            <x-input-label for="email" :value="__('Email')" class="form-label"/>
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="invalid-feedback" />
+        </div>
+
+        <!-- Password -->
+        <div class="col-12">
+            <x-input-label for="password" :value="__('Password')" class="form-label"/>
+
+            <x-text-input id="password" class="form-control"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="invalid-feedback" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="col-12">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label"/>
+
+            <x-text-input id="password_confirmation" class="form-control"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="invalid-feedback" />
+        </div>
+
+        
+        <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Already have an account? <a href="pages-login.html">Log in</a></p>
+                    </div>
+    </form>
+</x-guest-layout>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Laravel ADV</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      Modified  by <a href="https://bootstrapmade.com/">Andrey Vega</a>
+    </div>
+  </footer>
